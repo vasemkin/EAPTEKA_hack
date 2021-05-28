d7726bcc73d038862d106661957358f047c34019
vasemkin <vasemkin@ya.ru>
2021-05-29 00:12:10 +0300

    ai ai ye coco jamboo

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 MAD Team EAPTEKA Hack 28.05.2021
+https://www.notion.so/eapteka-backend-91250775e5df453eb289a89ed2af202b
--- backend/app/routes/models.js
+++ backend/app/routes/models.js
@@ -2,12 +2,23 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-let userModel = new Schema(
+const user = new Schema(
   {
     uuid : String,
-    type : String
+    type : String,
+    prescriptions : Array
   },
   { collection: "User" }
 );
 
-module.exports = mongoose.model("User", userModel);
+const prescription = new Schema({
+  drugs : Array,
+  uuid : String,
+  description : String,
+  doctor_id : String 
+  },
+  { collection : "Prescription" }
+)
+
+module.exports = mongoose.model("User", user);
+module.exports = mongoose.model("Prescription", prescription);
